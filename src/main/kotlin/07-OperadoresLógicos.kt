0fbd8f3f11731e473688d4951972c4a0322d6658
Cronuzgamer <gabrieljknd@gmail.com>
2022-11-10 18:29:43 -0300

    Update

--- src/main/kotlin/07-OperadoresLógicos.kt
+++ src/main/kotlin/07-OperadoresLógicos.kt
@@ -4,8 +4,20 @@ fun main (){
     println("&& E")
     println("|| Ou")
 
+   /*quando se usa o operador logico && se um dos lados da condicao for falsa ele ira ser falso *//*utilize o && pra
+   comparar coisas é igual?*/
 
+//    if (20==20 && 10 < 20){
+//        println("Verdadeiro")
+//    }else{
+//        println("Falso")
+//    }
 
-
+    /*quando um dos lados for falso com o || Ou , dara sempre verdadeiro inverso do operador logico && */
+    if (20==20 || 10 < 20){
+        println("Verdadeiro")
+    }else{
+        println("Falso")
+    }
 
 }
