559e7d68b993be99c81ba574b3355ddcf851441b
Cronuzgamer <gabrieljknd@gmail.com>
2022-11-10 23:54:09 -0300

    Update

--- src/main/kotlin/09-EstruturasDeRepetiçãoWhile.kt
+++ src/main/kotlin/09-EstruturasDeRepetiçãoWhile.kt
@@ -1,7 +1,7 @@
 fun main(){
 
 
-    println("*** Estruturas de Repetição ***")
+    println("*** Estruturas de Repetição While ***")
 
     //While Enquanto//
     var listaCompras = arrayOf("Arroz","Feijão","Batata","Uva","Banana","Carne","Leite","Ovos","Frango")
---
+++ src/main/kotlin/10-EstruturaDeRepetiçãoFor.kt
@@ -0,0 +1,14 @@
+fun main (){
+
+    println("Estrutura de repetição For")
+    var listaCompras = arrayOf("Arroz","Feijão","Batata","Uva","Banana","Carne","Leite","Ovos","Frango")
+    var indice = 0
+                // For = Do tal ao Tal no caso do 0 ao 8 usando os .. para selecionar//
+                // in igual Intervalo //
+    for (indice in 0..listaCompras.size -1){
+        println(listaCompras[indice])
+
+   }
+
+
+    }
