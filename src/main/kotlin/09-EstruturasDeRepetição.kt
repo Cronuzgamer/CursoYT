acf1c30a5afc0fece576339537fbef70e649be8e
Cronuzgamer <gabrieljknd@gmail.com>
2022-11-10 23:46:21 -0300

    Update

---
+++ src/main/kotlin/09-EstruturasDeRepetição.kt
@@ -0,0 +1,19 @@
+fun main(){
+
+
+    println("*** Estruturas de Repetição ***")
+
+    //While Enquanto//
+    var listaCompras = arrayOf("Arroz","Feijão","Batata","Uva","Banana","Carne","Leite","Ovos","Frango")
+
+    var indice = 0
+
+    //estrutura de repeticao (enquanto) (Condiçao do While) Enquanto for verdadeiro sera executado ,se tornar falso
+    //para de ser executado//
+    while (indice <=listaCompras.size -1){
+        println(listaCompras[indice])
+        indice++
+
+    }
+
+}
