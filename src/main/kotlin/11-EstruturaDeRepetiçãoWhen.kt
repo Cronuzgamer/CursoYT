1d725d160e8070df998c260524ec579c10ab14fb
Cronuzgamer <gabrieljknd@gmail.com>
2022-11-16 18:53:58 -0300

    Update

--- src/main/kotlin/11-EstruturaDeRepetiçãoWhen.kt
+++ src/main/kotlin/11-EstruturaDeRepetiçãoWhen.kt
@@ -1,22 +1,22 @@
-fun main (){
+    fun main (){
 
-    println(" Estrutura de repetição When /Quando")
+        println(" Estrutura de repetição When /Quando")
 
-    var opcao= 1
+        var opcao= 1
 
-    //Estrutura condicional codigo mais limpo//
-    when(opcao){
-        1 -> println("Suco de Laranja")
-        2 -> println("Suco de Uva")
-        3 -> println("Suco de Morango")
-        else -> println("Nenhuma opção foi selecionada")
+        //Estrutura condicional codigo mais limpo//
+        when(opcao){
+            1 -> println("Suco de Laranja")
+            2 -> println("Suco de Uva")
+            3 -> println("Suco de Morango")
+            else -> println("Nenhuma opção foi selecionada")
+        }
+
+        var mensagem = when (opcao){
+            1 -> "Suco de Laranja"
+            2 -> "Suco de Uva"
+            3 -> "Suco de Morango"
+            else ->"Opção incorreta"
+        }
+        println(mensagem)
     }
-
-    var mensagem = when (opcao){
-        1 -> "Suco de Laranja"
-        2 -> "Suco de Uva"
-        3 -> "Suco de Morango"
-        else ->"Opção incorreta"
-    }
-    println(mensagem)
-}
